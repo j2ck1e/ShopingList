94c200f51055030d472061f64395b790e9238e28
j2ck1e <j2ck1e86@gmail.com>
2023-05-16 23:14:26 +0300

    Added ShopListAdapter

---
+++ app/src/main/java/com/jcdesign/shopinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,35 @@
+package com.jcdesign.shopinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.jcdesign.shopinglist.R
+import com.jcdesign.shopinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+        viewHolder.itemView.setOnLongClickListener { true }
+    }
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+}
